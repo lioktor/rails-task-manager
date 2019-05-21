383ddaee355375ab1f42eaf95ae406d40a1adb24
Ktorza <lionel.ktorza@gmail.com>
2019-05-21 18:06:21 +0200

    fgfgty

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to show_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
